624c42ec4ee59cf7084891b8d37b8fa08ce92fe7
Wang Lijun <wang283808065@gmail.com>
2017-09-26 16:57:44 -0700

    7 restful routes complete

--- app.js
+++ app.js
@@ -98,6 +98,17 @@ app.put("/blogs/:id", function(req, res){
     })
 });
 
+//DELETE ROUTE
+app.delete("/blogs/:id", function(req, res){
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if (err){
+            res.redirect("/blogs");
+        }else{
+            res.redirect("/blogs");
+        }
+    })
+})
+
 app.listen(7000, function(){
     console.log("BLOG POST has started");
 })
